4e32538ef543d4fb4d02082b6445a5eb613db6e1
Sagiri18 <48347578+Sagiri18@users.noreply.github.com>
2021-03-12 20:16:44 +0800

    modify

--- lib/make.sh
+++ lib/make.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 
-CUDA_PATH=/usr/local/cuda/
+export CPATH=/usr/local/cuda/include
+export CUDA_PATH=/usr/local/cuda/
+export PATH=/usr/local/cuda/bin/:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+export CXXFLAGS="-std=c++11"
+export CFLAGS="-std=c99"
+
 
 #python setup.py build_ext --inplace
 #rm -rf build
 
-CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
-           -gencode arch=compute_35,code=sm_35 \
-           -gencode arch=compute_50,code=sm_50 \
-           -gencode arch=compute_52,code=sm_52 \
-           -gencode arch=compute_60,code=sm_60 \
-           -gencode arch=compute_61,code=sm_61 "
+CUDA_ARCH="-gencode arch=compute_75,code=sm_75 "
 
 # compile NMS
 echo "Building nms op..."
-cd ./model/nms
+%cd /content/R-C3D.pytorch-pytorch-1.1/lib/model/nms
 if [ -d "build" ]; then
     rm -r build
 fi
-python setup.py build_ext --inplace
+python /content/R-C3D.pytorch-pytorch-1.1/lib/model/nms/setup.py build_ext --inplace
 
 # compile roi_temporal_pool
 echo "Building roi_temporal_pool"
-cd ../roi_temporal_pool
+%cd /content/R-C3D.pytorch-pytorch-1.1/lib/model/roi_temporal_pool
 if [ -d "build" ]; then
     rm -r build
 fi
-python setup.py build_ext --inplace
+python /content/R-C3D.pytorch-pytorch-1.1/lib/model/roi_temporal_pool/setup.py build_ext --inplace
